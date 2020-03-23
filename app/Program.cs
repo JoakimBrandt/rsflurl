4deedda75aea4dad02d285153c57a41f8c00ce45
Fredric Lundberg <Fredric.idk@outlook.com>
2020-03-23 17:19:42 +0100

    add parameter in fetchasync

--- app/Program.cs
+++ app/Program.cs
@@ -12,25 +12,28 @@ namespace app
 {
     class Program
     {
+        private static int _amountOfCallsArrPosition = 0;
+        private static int _amountOfDataPerCallArrPosition = 1;
+        private static int _typeOfClientArrPosition = 2;
+
         public static void Main(string[] args)
         {
-            Array array = menuChoices();
+            string[] arrayChoices = menuChoices();
 
             try
             {
-                fetch().Wait();
+                //ha inparameter i fetch()
+                fetch(arrayChoices).Wait();
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"There was an exception: {ex.ToString()}");
-
             }
-            
         }
 
-        private static Array menuChoices()
+        private static string[] menuChoices()
         {
-            string[] choices = new string[3];
+            string[] choicesArr = new string[3];
 
             Console.WriteLine("How many anrop do you want to send? Type 1, 8, 64, 128...: ");
             var amountOfCalls = Console.ReadLine();
@@ -44,23 +47,51 @@ namespace app
             var typeOfClient = Console.ReadLine();
             Console.WriteLine("You entered '{0}'", typeOfClient);
 
-            choices[0] = amountOfCalls;
-            choices[1] = amountOfDataPerCall;
-            choices[2] = typeOfClient;
+            choicesArr[0] = amountOfCalls;
+            choicesArr[1] = amountOfDataPerCall;
+            choicesArr[2] = typeOfClient;
 
-            return choices;
+            return choicesArr;
         }
 
-        private async static Task fetch() {
-            Console.WriteLine("hello there");
- 
-            var response = await "https://api.postcodes.io"
-                .AppendPathSegment("postcodes")
-                .AppendPathSegment("IP1 3JR")
-                .GetJsonAsync();
-            
-            Console.WriteLine("API hämtar postcoden från json objektets resultat:" + response.result.postcode);
 
+        private async static Task fetch(string[] choicesArr) {            
+            int amountOfCalls;
+            bool success = int.TryParse(choicesArr[_amountOfCallsArrPosition], out amountOfCalls);
+
+            try
+            {
+                string typeOfClient = choicesArr[_typeOfClientArrPosition];
+                
+                if(typeOfClient == "RS") {
+                    if(success) {
+                        //TODO skapa en funktion som kör x-antal loops
+                        for (int i = 0; i < amountOfCalls; i++)
+                        //TODO starta upp en tråd för varje anrop
+                        {
+                            var response = await "https://api.postcodes.io"
+                            .AppendPathSegment("postcodes")
+                            .AppendPathSegment("IP1 3JR")
+                            .GetJsonAsync();
+                        
+                            Console.WriteLine("API hämtar postcoden från json objektets resultat:" + response.result.postcode);
+                        }
+                    }
+                } else if(typeOfClient == "FL") {
+                    //TODO skapa en funktion som kör x-antal loops
+                    //TODO run FLurl http-client here
+                } else { 
+                    Console.WriteLine("You have not choosed any HTTP-CLient");
+                }
+            }
+            
+            catch (Exception ex)
+            {
+                Console.WriteLine($"There was an exception: {ex.ToString()}");
+                throw;
+            }
+
+        
             //-------------------------
 
             var client = new RestClient("https://api.postcodes.io");
