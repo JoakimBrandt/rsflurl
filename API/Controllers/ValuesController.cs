226973ba78abc60f367f1db303e8e15795705e38
JoakimBrandt <brjo1621@student.ju.se>
2020-03-25 19:55:20 +0100

    Overhauled and re-did whole API
    Is now a working, fully functional API solution, but more tinkering is needed to optimize the experience.

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MySql.Data.MySqlClient;
+
+namespace API.Controllers
+{
+    public class ValuesController : ApiController
+    {
+
+        public class Results
+        {
+            public string name { get; set; }
+            public string department { get; set; }
+            public string address { get; set; }
+            public string number { get; set; }
+
+            public Results(string name, string department, string address, string number)
+            {
+                this.name = name;
+                this.department = department;
+                this.address = address;
+                this.number = number;
+            }
+
+        }
+
+        // GET api/values
+        public List<Results> Get()
+        {
+            MySqlConnection connection = WebApiConfig.Connection();
+
+            MySqlCommand query = connection.CreateCommand();
+
+            query.CommandText = "SELECT * FROM data.employees;";
+
+            var Results = new List<Results>();
+
+            try
+            {
+                connection.Open();
+            }
+            catch (MySqlException Exception)
+            {
+                throw Exception;
+            }
+
+            MySqlDataReader fetchQuery = query.ExecuteReader();
+
+            while ( fetchQuery.Read())
+            {
+                Results.Add(new Results(
+                    fetchQuery["Name"].ToString(), 
+                    fetchQuery["Department"].ToString(), 
+                    fetchQuery["Address"].ToString(), 
+                    fetchQuery["Number"].ToString())
+                    );
+            }
+
+            return Results;
+        }
+    }
+}
--- API/Models/Employee.cs
+++
@@ -1,9 +0,0 @@
-public class Employee {
-
-    public long EmployeeId { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string Phone { get; set; }
-    public string Address { get; set; }
-
-}
